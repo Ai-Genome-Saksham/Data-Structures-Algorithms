cdc44b5ba28d46f033c58077818ef58fda28922d
Ai-Genome-Saksham <88901517+Ai-Genome-Saksham@users.noreply.github.com>
2022-06-12 09:54:52 +0530

    Create 3a Frog Jump.cpp

---
+++ 10. Dynamic Programming/3a Frog Jump.cpp
@@ -0,0 +1,22 @@
+/*
+https://www.codingninjas.com/codestudio/problems/frog-jump_3621012?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+*/
+
+#include<bits/stdc++.h>
+
+int f(int ind, vector<int> &heights, vector<int> &dp){
+    
+    if(ind==0) return 0;
+    if(dp[ind] != -1) return dp[ind];
+    int left = f(ind-1, heights, dp) + abs(heights[ind]-heights[ind-1]);
+    int right = INT_MAX;
+    if(ind>1) right = f(ind-2, heights, dp) + abs(heights[ind]-heights[ind-2]);
+    
+    return dp[ind]=min(left, right);
+}
+
+int frogJump(int n, vector<int> &heights)
+{
+    vector<int> dp(n,-1);
+    return f(n-1, heights, dp);
+}
